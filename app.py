675f889a5c15534a587f8798a0acb3896ffd7ad6
isvar <ongorio5@gmail.com>
2021-08-28 14:33:34 -0500

    custom error

--- app.py
+++ app.py
@@ -13,8 +13,28 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 
+# Log In
+from flask_login import (
+                        UserMixin, login_manager, login_user, LoginManager,
+                        login_required, logout_user, current_user,
+)
+
 # Clases camelCase
 # todo lo demas snake_case
+
+
+
+
+# Custom Error Pages
+@app.errorhandler(404):
+def error_404_handler(e):
+    return render_template('404.html')
+
+
+@app.errorhandler(500):
+def error_500_handler(e):
+    return render_template('500.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
