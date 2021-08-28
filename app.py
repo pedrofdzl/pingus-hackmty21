f6282152b04b96c51f78e62dc13968c537f4d2a2
alemendoza-v <aletinmendoza@hotmail.com>
2021-08-28 16:21:39 -0500

    Class implementation and test

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from sqlalchemy.orm import backref
-
+from sqlalchemy.ext.mutable import MutableList
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pingus.db'
@@ -27,13 +27,67 @@ from flask_login import (
 
 # Clases camelCase
 class Class(db.Model):
+    __tablename__ = 'parent' 
     id = db.Column(db.Integer, nullable=False, primary_key=True)
-    name = db.Column(db.String, nullable=False)
+    name = db.Column(db.Text, nullable=False)
     quizes = db.relationship('Quiz', backref='Quiz', lazy=True)
-    activities = db.relationship('Activity', backref='Activity', lazy=True)
-    forum = db.relationship('Forum', backref='Forum', uselist=False)
-    lectures = db.relationship('Lecture', backref='Lecture', lazy='True')
-    projects = db.relationship('Project', backref='Project', lazy=True)
+    #activities = db.relationship('Activity', backref='Activity', lazy=True)
+    #forum = db.relationship('Forum', backref=backref('Forum', uselist='False'))
+    #lectures = db.relationship('Lecture', backref='Lecture', lazy=True)
+    #projects = db.relationship('Project', backref='Project', lazy=True)
+
+    def __repr__(self):
+        return 'Class ' + str(self.id) 
+
+class Quiz(db.Model):
+    __tablename__ = 'child'
+    id = db.Column(db.Integer, nullable=False, primary_key=True)
+    name = db.Column(db.Text, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    #questions = db.relationship('Question', back_populates='Question', lazy=True)
+    parent_id = db.Column(db.Integer, db.ForeignKey('parent.id'))
+
+    def __repr__(self):
+        return 'Quiz ' + str(self.id) 
+'''
+class Activity(db.Model):
+    __tablename__ = 'child'
+    id = db.Column(db.Integer, nullable=False, primary_key=True)
+    name = db.Column(db.Text, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    datePublished = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    dateDue = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    parent_id = db.Column(db.Integer, db.ForeignKey('parent.id'))
+
+    def __repr__(self):
+        return 'Activity ' + str(self.id) 
+
+class Lecture(db.Model):
+    __tablename__ = 'child'
+    id = db.Column(db.Integer, nullable=False, primary_key=True)
+    name = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    parent_id = db.Column(db.Integer, db.ForeignKey('parent.id'))
+
+    def __repr__(self):
+        return 'Lecture ' + str(self.id) 
+'''
+
+'''
+class Question(db.Model):
+    __tablename__ = 'child'
+    id = db.Column(db.Integer, nullable=False, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    weight = db.Column(db.Float, nullable=False)
+    answers = db.Column(MutableList.as_mutable(db.PickleType), default=[])
+    parent_id = db.Column(db.Integer, db.ForeignKey('parent.id'))
+
+    def __repr__(self):
+        return 'Question ' + str(self.id) 
+
+'''
 
 # todo lo demas snake_case
 
